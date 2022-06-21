ba258385bb16d2e3d3243a21db56d23d04d5ce11
SrOliver202023 <sroliver202023@gmail.com>
2022-06-20 22:01:54 -0300

    :truck: Renaming ./src/Screens to ./src/screens

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { GlobalStyle } from "./global";
-import { Home } from "./Screen/Home";
+import { Home } from "./screens/Home";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
